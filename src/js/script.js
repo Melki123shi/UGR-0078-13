712ef1fff33649d74b902019992489f7a8c83ed1
Melkishi Tesfaye <melkishitesafu@gmail.com>
2023-01-17 15:45:51 -1200

    Revert "Add files via upload"
    This reverts commit c1160f2e9a86407255faf09662195ca1861e3275.

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-const toggleButton = document.getElementsByClassName('tog-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
-
